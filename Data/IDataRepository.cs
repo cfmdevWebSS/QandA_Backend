23ceb766a7b20e4d32d643e188f04a93b331f058
Lawrence Buta <larry.buta@gmail.com>
2022-02-27 15:55:17 -0500

    Interim Save-Chapter8-ASP.NET5-02-26-2022-Rev3

--- Data/IDataRepository.cs
+++ Data/IDataRepository.cs
@@ -4,11 +4,22 @@ namespace QandA.Data
 {
     public interface IDataRepository
     {
+        #region readdata
         IEnumerable<QuestionGetManyResponse> GetQuestions();
         IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search);
         IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
         QuestionGetSingleResponse GetQuestion(int questionId);
         bool QuestionExists(int questionId);
         AnswerGetResponse GetAnswer(int answerId);
+        #endregion
+
+        #region writedata
+        QuestionGetSingleResponse PostQuestion(QuestionPostRequest question);
+        QuestionGetSingleResponse PutQuestion(int questionId, QuestionPutRequest question);
+        void DeleteQuestion(int questionId);
+        AnswerGetResponse PostAnswer(AnswerPostRequest answer);
+
+
+        #endregion
     }
 }
